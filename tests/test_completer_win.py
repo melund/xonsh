dd8c09f9663ce4eb768b03b3e2f09341d4879c80
mel <mel@anybodytech.com>
2015-08-06 13:58:00 +0200

    More exploration on path completion on windows

--- tests/test_completer_win.py
+++ tests/test_completer_win.py
@@ -1,7 +1,7 @@
 """Tests the xonsh lexer."""
 from __future__ import unicode_literals, print_function
 import os
-import contextlib 
+from contextlib import ContextDecorator, contextmanager
 import tempfile
 
 
@@ -14,55 +14,56 @@ built_ins.load_builtins()
 
 from xonsh.tools import ON_WINDOWS
 
-@contextlib.contextmanager
-def setup_folder():
-   with  tempfile.TemporaryDirectory() as tmpdir:
-       os.chdir(tmpdir)
-       os.makedirs('folder with space/subfolder1')
-       os.makedirs('folder with space/subfolder2')
-       os.makedirs('folder with space2/subfolder')
-       yield tmpdir
+
+
+@contextmanager
+def setup_test_dir():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        cwd = os.getcwd()        
+        os.chdir(tmpdir)
+        os.makedirs('Folder WITH Spaces/subfolder1')
+        os.makedirs('Folder WITH Spaces/subfolder2')
+        yield tmpdir
+        os.chdir(cwd)
 
 
 if ON_WINDOWS:
     
-    @setup_folder
     def test_complete1():
-        cmpl = Completer()
-        args = ('folder', "cd folder",3,8)
-        cmpl_list = cmpl.complete(*args)
-        assert_equal(cmpl_list[0], "'folder with space\\'")
+        with setup_test_dir():
+            cmpl = Completer()
+            args = ('folder', "cd folder",3,8)
+            cmpl_list = cmpl.complete(*args)
+            assert_equal(cmpl_list[0], "'Folder WITH Spaces\\'")
     
     def test_complete2():
-        cmpl = Completer()
-        args = ("'wit", "cd 'folder wit",11,13)
-        cmpl_list = cmpl.complete(*cmplwrap(*args))
-        assert_equal(cmpl_list[0], "'folder with space\\'")
-        
-        args = ('"fold', 'cd "fold',3,7)
-        cmpl_list = cmpl.complete(*cmplwrap(*args))
-        assert_equal(cmpl_list[0], "'folder with space\\'")
-        
-        args = ('s', 'cd "folder with s',16,17)
-        cmpl_list = cmpl.complete(*cmplwrap(*args))
-        assert_equal(cmpl_list[0], "'folder with space\\'")
+        with setup_test_dir():
+            cmpl = Completer()
+            args = ("wit", "cd 'folder wit",11,13)
+            cmpl_list = cmpl.complete(*args)
+            assert_equal(cmpl_list[0], "'Folder WITH Spaces\\'")
+            
+            args = ('"fold', 'cd "fold',3,7)
+            cmpl_list = cmpl.complete(*args)
+            assert_equal(cmpl_list[0], "'Folder WITH Spaces\\'")
+            
+            args = ('s', 'cd "folder with s',16,17)
+            cmpl_list = cmpl.complete(*args)
+            assert_equal(cmpl_list[0], "'Folder WITH Spaces\\'")
+
+            args = ('s', 'cd "./folder with s',16,17)
+            cmpl_list = cmpl.complete(*args)
+            assert_equal(cmpl_list[0], "'Folder WITH Spaces\\'")
 
         # make something that completes part of a filename/cmd/etc. if the suggested
         # autocompletions have a some prefix in common. 
 
 
-def cmplwrap( prefix, line, begidx, endidx):
-    """ Expand the prefix backwards on the line to the first
-        quote character (",') found, and change begidx accordingly """
-    quote_idxs = map(line[0:begidx].rfind, ('"',"'"))
-    if quote_idxs:
-        begidx = max(quote_idxs)
-    
-    prefix = line[begidx:endidx+1]
-    return (prefix, line, begidx, endidx)
-    
+
 
 if __name__ == '__main__':
+    
+    
     
     test_complete1()
     test_complete2()
